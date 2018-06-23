7581a43eb51946d4cf6d408b9a7e79d2a4bf8eaa
trubasa <1141521502@qq.com>
2018-06-23 14:13:13 +0800

    1

---
+++ src/lib/vuePictureManager.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="vue-picture-manager">
+    hello world,vue-picture-manager!
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "vue-picture-manager"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import App from './App.vue'
+import vuePictureManager from './lib/index.js'
+
+Vue.use(vuePictureManager);
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
